b077a7d875805954abec70133d364e565fd8d877
Are Digranes <ared@runbox.no>
2023-10-25 19:15:00 +0200

    apple_music_api_4.0_beta2

--- Apple Music (API)/mp3tag_proxy.py
+++ Apple Music (API)/mp3tag_proxy.py
@@ -1,5 +1,5 @@
 # Apple Music proxy for Apple Music Web Source 4.0 for MP3Tag
-# Version 1.0 © 2023
+# Version 1.0b2 © 2023
 # All Rights Reserved
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
@@ -7,7 +7,8 @@ from ssl import wrap_socket
 import requests
 
 TOKEN = "Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IldlYlBsYXlLaWQifQ.eyJpc3MiOiJBTVBXZWJQbGF5IiwiaWF0IjoxNjkyNjM5MTM1LCJleHAiOjE2OTk4OTY3MzUsInJvb3RfaHR0cHNfb3JpZ2luIjpbImFwcGxlLmNvbSJdfQ.nJ_cqCk2lRPbzK7jxHXwu1aGaxGiMYa0px0DdAfI_9GcUuB5Ki_lKO6IfWEgxA5Dr9_KAh81ACpHkcgXjh8ArQ"
-URL = "https://api.music.apple.com"
+USER = ""
+URL = "https://amp-api.music.apple.com"
 PORT = 8084
 
 session = requests.Session()
@@ -17,9 +18,10 @@ session.headers.update(
 				"User-Agent": "Mozilla/5.0 (Proxy 1.0)",
 				"Accept": "application/json",
 				"Authorization": TOKEN,
+				"media-user-token": USER,
 				"Content-Type": "application/json",
 				"Connection": "keep-alive",
-				"Origin": "https://music.apple.com",
+				"Origin": "https://music.apple.com"
 		}
 )
 
